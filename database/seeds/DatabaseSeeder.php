7e083d7a83a2d35d3d5e8edb7aa85cd261950fe4
Axel Espinosa <axeldavid45@gmail.com>
2020-04-13 11:50:28 -0500

    Configuramos el seeder de la bd

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,28 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
+        //Create the admin user
+        User::create(
+            [
+                'name'          => 'Axel Espinosa',
+                'email'         => 'axeldavid45@gmail.com',
+                'phone'         => '9611581478',
+                'password'      => Hash::make('123456789'),
+                'administrator' => true
+            ]
+        );
+
+        //Creating the cashier
+        User::create(
+            [
+                'name'          => 'Cajero 1',
+                'email'         => 'cajero@cajero.com',
+                'phone'         => '9615545454',
+                'password'      => Hash::make('123456789'),
+                'administrator' => false
+            ]
+        );
+        factory(App\Category::class, 2)->create();
+        factory(App\Product::class, 10)->create();
     }
 }
